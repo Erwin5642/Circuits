6cb29cf3ecd5c471c7577b16f5900b87216841e1
Lain <joaovitor14mju@gmail.com>
2024-11-02 19:01:05 -0400

    Cmake

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/murak/OneDrive/Documentos/SFML/Circuits
+# Install script for directory: C:/Users/jvgam/CLionProjects/Circuits
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/murak/OneDrive/Documentos/SFML/Circuits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/jvgam/CLionProjects/Circuits/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
